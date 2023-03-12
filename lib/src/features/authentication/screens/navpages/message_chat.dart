a6b7dfd1ffb2257226a795b46814ca819545df2e
Myra Lugwiri <m.lugwiri@alustudent.com>
2023-03-12 02:32:54 +0200

    Added the message chat model

---
+++ lib/src/features/authentication/screens/navpages/message_chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class messagePage extends StatefulWidget {
+  @override
+  State<messagePage> createState() => _messagePage();
+}
+
+class _messagePage extends State<messagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
